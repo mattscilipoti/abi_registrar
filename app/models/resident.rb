d4aaee5492055704bb5773f44722ac2728b83e5f
Matt Scilipoti <matt@scilipoti.name>
2022-04-18 00:12:49 -0400

    Resident encrypts age_of_minor, email_address, first_name
    - validates age_of_minor

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,2 +1,7 @@
 class Resident < ApplicationRecord
+  encrypts :age_of_minor
+  encrypts :email_address, deterministic: true
+  encrypts :first_name, deterministic: true
+
+  validates :age_of_minor, numericality: { integer: true, less_than: 14 }
 end
