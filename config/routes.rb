232eba0c4693c2bbf1271e77ac83b28469a40f65
Matt Scilipoti <matt@scilipoti.name>
2022-04-17 23:58:06 -0400

    Nav: add Residents to menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :residents
-  resources :properties
-  resources :lots
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  resources :lots
+  resources :properties
+  resources :residents
 
   # Defines the root path route ("/")
   # root "articles#index"
