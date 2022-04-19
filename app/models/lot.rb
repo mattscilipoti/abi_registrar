ceaa5cb768970829627c125ca7c7fb8556cf962f
Matt Scilipoti <matt@scilipoti.name>
2022-04-19 00:51:39 -0400

    Convert Lots#indes to table w/slim

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -6,4 +6,9 @@ class Lot < ApplicationRecord
   validates :account_number, numericality: { only_integer: true }
   validates :section, numericality: { only_integer: true, in: 1..5 }
   validates :size, inclusion: { in: [0.5, 1] }
+
+  def tax_identifier
+    formatted_district = format('%02d', district)
+    [formatted_district, subdivision, account_number].join(' ')
+  end
 end
--- test/models/lot_test.rb
+++ test/models/lot_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class LotTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'tax_id concatenates district, subdivisoin, and account_number' do
+    l = Lot.new(district: 1, subdivision: 123, account_number: 234567)
+    assert_equal '01 123 234567', l.tax_id
+  end
 end
