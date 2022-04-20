9ba7e6c9c1ce34d2b66a032085bb3b1e8edbdb37
Matt Scilipoti <matt@scilipoti.name>
2022-04-19 23:24:38 -0400

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Residency.destroy_all
+Resident.destroy_all
+Lot.destroy_all
+Property.destroy_all
+
+lot69 = FactoryBot.create(:lot, lot_number: 69)
+property_975 = FactoryBot.create(:property, lots: [lot69], street_number: '975', street_name: 'Waterview Dr')
+
+mms = FactoryBot.create(:resident, last_name: 'Scilipoti', first_name: 'Matt', email_address: 'matt@scilipoti.name', properties: [property_975]).tap do |resident|
+  resident.residencies.first.update(
+    resident_status: :deed_holder,
+    verified_on: 1.day.ago
+  )
+end
+
+cbs = FactoryBot.create(:resident, last_name: 'Scilipoti', first_name: 'Cindy', email_address: 'cindy@scilipoti.name', properties: [property_975]).tap do |resident|
+  resident.residencies.first.update(
+    resident_status: :deed_holder,
+    verified_on: 1.day.ago
+  )
+end
+
+jhs = FactoryBot.create(:resident, :minor, last_name: 'Scilipoti', first_name: 'J', email_address: 'jh@scilipoti.name', age_of_minor: 17, properties: [property_975]).tap do |resident|
+  resident.residencies.first.update(
+    resident_status: :dependent,
+    verified_on: 1.day.ago,
+  )
+end
+
+lot70 = FactoryBot.create(:lot, lot_number: 70)
+lot71 = FactoryBot.create(:lot, lot_number: 71)
+property_971 = FactoryBot.create(:property, lots: [lot70, lot71], street_number: '971', street_name: 'Waterview Dr')
+
+jr = FactoryBot.create(:resident, last_name: 'Rainwater', first_name: 'Jim', email_address: 'jim@example.com', properties: [property_971]).tap do |resident|
+  resident.residencies.first.update(
+    resident_status: :deed_holder,
+    verified_on: 1.day.ago)
+end
+
+lot42 = FactoryBot.create(:lot, lot_number: 42)
+property_123 = FactoryBot.create(:property, lots: [lot42], street_number: '123', street_name: 'Main St')
+
+jqo = FactoryBot.create(:resident, last_name: 'Owner', first_name: 'Jane', email_address: 'janeowner@example.com', properties: [property_123]).tap do |resident|
+  resident.residencies.first.update(
+    resident_status: :deed_holder,
+    verified_on: 1.day.ago
+  )
+end
+
+jqr = FactoryBot.create(:resident, last_name: 'Renter', first_name: 'John Q.', email_address: 'johnqrenter@example.com', properties: [property_123]).tap do |resident|
+  resident.residencies.first.update(
+    resident_status: :renter,
+    verified_on: 1.day.ago
+  )
+end
--- test/factories/properties.rb
+++ test/factories/properties.rb
@@ -3,12 +3,14 @@ FactoryBot.define do
     street_number { Faker::Address.building_number }
     street_name { Faker::Address.street_name }
     
-    transient do
-      lots_count { 1 }
-    end
-    
-    after(:create) do |property, evaluator|
-      create_list(:lot, evaluator.lots_count, property: property)
+    trait :with_lots do
+      transient do
+        lots_count { 1 }
+      end
+      
+      after(:create) do |property, evaluator|
+        create_list(:lot, evaluator.lots_count, property: property)
+      end
     end
   end
 end
