1908ac896a1e59f5986c1d9a2c3561c7b47cb518
Matt Scilipoti <matt@scilipoti.name>
2022-04-19 21:51:56 -0400

    Style: menu
    - add root route

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def menu_list_item(caption, url_options, html_options={})
+    default_css_classes = []
+    default_css_classes << 'is-active' if current_page?(url_options)
+    default_html_options = { class: default_css_classes}
+    content_tag(:li, default_html_options) do
+      link_to caption, url_options, html_options
+    end
+  end
 end
