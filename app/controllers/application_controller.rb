4e16687dbe3ccac1240c9c9621514aee8573430d
Matt Scilipoti <matt@scilipoti.name>
2022-05-11 00:04:03 -0400

    Sort: move sort_models to helper
    - only used in view now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,3 @@
 class ApplicationController < ActionController::Base
 
-  private
-
-  def sort_models(models, sort_info)
-    sort_column = sort_info[:column]
-    sort_direction = sort_info[:direction]
-    # models.order("#{sort_column} #{sort_direction}")
-
-    # WORKAROUND: support sorting by non-AR columns
-    # Yes, this is inefficient but our lists are small
-    # Using format(), with leading zeros, to support sorting string, number, or TrueClass
-    models = models.sort_by{|r| format('%010s' % r.send(sort_column).to_s)}
-    if sort_direction.to_s != 'asc'
-      models = models.reverse
-    end
-    models
-  end
-
-  helper_method :sort_models
 end
