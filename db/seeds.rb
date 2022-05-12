31358bfd06a15761d3903da6ca233dcdd5aebe6b
Matt Scilipoti <matt@scilipoti.name>
2022-05-12 14:34:36 -0400

    seed: test_admin with credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -78,6 +78,13 @@ FactoryBot.create(:share_transaction, :transfer, quantity: 10, from_residency: j
 
 Resident.lot_fees_paid.each {|r| FactoryBot.create(:vehicle, resident: r) }
 
+test_admin_info = Rails.application.credentials.fetch(:test_admin)
+Account.create!(
+  email: test_admin_info.fetch(:email),
+  password_hash: BCrypt::Password.create(test_admin_info.fetch(:password)).to_s,
+  status:     2, # verified
+)
+
 Account.create!(
   email: 'registrar@ardenbeachesinc.com',
   password_hash: BCrypt::Password.create("change_me").to_s,
