d1eae619479a1b755b45827571f564662f9ad76c
Matt Scilipoti <matt@scilipoti.name>
2022-05-15 23:57:47 -0400

    Resident#middle_name added to forms

--- test/factories/residents.rb
+++ test/factories/residents.rb
@@ -3,6 +3,7 @@ require 'faker'
 FactoryBot.define do
   factory :resident do
     first_name { Faker::Name.first_name }
+    # middle_name { Faker::Name.middle_name }
     last_name { Faker::Name.last_name }
     email_address { Faker::Internet.email }
 
