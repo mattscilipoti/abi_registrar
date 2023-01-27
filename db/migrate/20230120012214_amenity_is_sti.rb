9a1b15f1845c75d01710a1c05f5554a58e86babd
Matt Scilipoti <matt@scilipoti.name>
2023-01-27 03:56:48 +0000

    #24 amenties: migration is update proof

--- db/migrate/20230120012214_amenity_is_sti.rb
+++ db/migrate/20230120012214_amenity_is_sti.rb
@@ -3,8 +3,10 @@ class AmenityIsSti < ActiveRecord::Migration[7.0]
     add_column 'amenities', 'type', :string
 
     # All current Amenities are Vehicles
+    amenity_snapshot_class = Class.new(ApplicationRecord)
+    amenity_snapshot_class.table_name = 'amenities'
     say_with_time "Assigning type:'VehicleParkingPass' to ALL amenities" do
-      AmenityPass.update_all(type: VehicleParkingPass.name)
+      amenity_snapshot_class.update_all(type: VehicleParkingPass.name)
     end
   end
 end
