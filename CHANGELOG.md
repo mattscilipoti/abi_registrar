6ad7c10d0b9fc3d3fd3eabf33e190d5273c77226
Matt Scilipoti <matt@scilipoti.name>
2024-02-04 02:38:23 +0000

    89 user_fee: AmenityPass requires mandatory fees

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,7 +12,7 @@ In place of release version numbers, we organize via deploys to Production (by D
 - Add Property.user_fee_paid_on to DB
 - Add Property.mandatory_fees_paid?
 - Add to Property & Resident index
-- AmenityPass checks resident_paid_mandatory_fees
+- AmenityPass checks confirm_resident_paid_mandatory_fees
 - YearEnd resets user fee
 
 ## Upcoming: Move Lot Fees To Property
--- app/models/amenity_pass.rb
+++ app/models/amenity_pass.rb
@@ -4,7 +4,7 @@ class AmenityPass < ApplicationRecord
   belongs_to :resident
   has_many :properties, :through => :resident
 
-  validate :resident_paid_mandatory_fees
+  validate :confirm_resident_paid_mandatory_fees
   validates :sticker_number, uniqueness: true
 
   def self.scopes
@@ -70,23 +70,22 @@ class AmenityPass < ApplicationRecord
     end
   end
 
-  def resident_paid_lot_fees
+  def confirm_resident_paid_lot_fees
     unless resident.lot_fees_paid?
       errors.add(:resident, "must have paid lot fees")
     end
   end
 
-  def resident_paid_mandatory_fees
-    resident_paid_lot_fees
-    resident_paid_user_fee
+  def confirm_resident_paid_mandatory_fees
+    confirm_resident_paid_lot_fees
+    confirm_resident_paid_user_fee
   end
 
-  def resident_paid_user_fee
+  def confirm_resident_paid_user_fee
     unless resident.user_fee_paid?
       errors.add(:resident, "must have paid user fee")
     end
   end
-
 
   def tag
     [state_code, tag_number].compact.join('-')
