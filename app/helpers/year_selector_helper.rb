2befb85bfd44737c7b20bc8001c380c79ce92720
Matt Scilipoti <matt@scilipoti.name>
2025-11-05 01:16:56 +0000

    SeasonYear filter uses all years

--- app/helpers/year_selector_helper.rb
+++ app/helpers/year_selector_helper.rb
@@ -4,7 +4,9 @@ module YearSelectorHelper
   # Otherwise fall back to a default recent range (current year and previous 4 years).
   def year_options(model: nil, range: (AppSetting.current_season_year - 4)..AppSetting.current_season_year)
     if model && model.respond_to?(:available_years)
-      years = model.available_years
+      # Instead of updating season_years for each model, always display all years
+      # years = model.available_years
+      years = AmenityPass.available_years
     else
       years = range.to_a.reverse
     end
